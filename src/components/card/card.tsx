0ce779bd7f9aef138419eb39ed37b0e611c82ed8
AnnaSergeevnaIv <kara4eva.anya@gmail.com>
2025-07-10 15:53:17 +0300

    feat: convert Card class component to functional component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,21 +1,18 @@
-import { Component, type ReactNode } from 'react';
 import { type CharacterData } from '../../services/network-requests';
 import placeholder from '../../assets/placeholder.png';
 import { CARD_CONTAINER_CLASS, CARD_IMAGE_CLASS } from './Card.constants';
 
-export class Card extends Component<CharacterData> {
-  render(): ReactNode {
-    return (
-      <div className={CARD_CONTAINER_CLASS} data-testid="card">
-        <img
-          src={this.props.image ? this.props.image : placeholder}
-          alt={`${this.props.name} image`}
-          className={CARD_IMAGE_CLASS}
-        />
-        <h3 className="font-bold text-white">{this.props.name}</h3>
-        <p>{`Height: ${this.props.height}`}</p>
-        <p>{`Eye color: ${this.props.eye_color}`}</p>
-      </div>
-    );
-  }
+export function Card({ name, height, eye_color, image }: CharacterData) {
+  return (
+    <div className={CARD_CONTAINER_CLASS} data-testid="card">
+      <img
+        src={image ? image : placeholder}
+        alt={`${name} image`}
+        className={CARD_IMAGE_CLASS}
+      />
+      <h3 className="font-bold text-white">{name}</h3>
+      <p>{`Height: ${height}`}</p>
+      <p>{`Eye color: ${eye_color}`}</p>
+    </div>
+  );
 }
