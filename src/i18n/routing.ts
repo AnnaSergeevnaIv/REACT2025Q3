527d9cc01dd5d9e78ddc35b567e1955c89a8b1ef
AnnaSergeevnaIv <kara4eva.anya@gmail.com>
2025-08-07 15:58:32 +0300

    fix: routing file

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,12 +3,6 @@ import { createNavigation } from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['en', 'ru'],
   defaultLocale: 'en',
-  pathnames: {
-    '/': '/',
-    '/pathnames': {
-      ru: '/not-found',
-    },
-  },
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
